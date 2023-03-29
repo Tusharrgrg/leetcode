85139bf791d7ba696094cd07713ea9559a8e4e9a
Tushar Agrawal <91628757+Tusharrgrg@users.noreply.github.com>
2023-03-30 01:10:12 +0530

    Added solution - LeetHub

---
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int V = Integer.parseInt(str[0]);
+            int E = Integer.parseInt(str[1]);
+    
+            ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
+            for(int i=0;i<V;i++)
+            {
+                adj.add(new ArrayList<ArrayList<Integer>>());
+            }
+            
+            int i=0;
+            while (i++<E) {
+                String S[] = read.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                int w = Integer.parseInt(S[2]);
+                ArrayList<Integer> t1 = new ArrayList<Integer>();
+                ArrayList<Integer> t2 = new ArrayList<Integer>();
+                t1.add(v);
+                t1.add(w);
+                t2.add(u);
+                t2.add(w);
+                adj.get(u).add(t1);
+                adj.get(v).add(t2);
+            }
+            
+            int S = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            
+            int[] ptr = ob.dijkstra(V, adj, S);
+            
+            for(i=0; i<V; i++)
+                System.out.print(ptr[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    {
+        // Write your code here
+        int[]dis = new int[V];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        
+        pq.add(new int[]{0,S});
+        
+        dis[S] = 0;
+        
+        while(!pq.isEmpty()){
+            int[]node = pq.remove();
+            
+            int u = node[1];
+            int d = node[0];
+            
+            for(int i = 0; i<adj.get(u).size(); i++){
+                int v = adj.get(u).get(i).get(0);
+                int edw = adj.get(u).get(i).get(1);
+                
+                if(edw+d<dis[v]){
+                    dis[v]= edw+d;
+                    pq.add(new int[]{edw+d,v});
+                }
+                
+            }
+        }
+        
+        return dis;
+        
+    }
+}
+
